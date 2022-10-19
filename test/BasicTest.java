61783d25ca3abeeccc61f52c75608e1fc5920fd4
tjingu <tjingu@plusgym.jp>
2022-10-19 15:12:20 +0900

    チュートリアル5 タグ機能のサポート ブログ UI へのタグの追加まで

--- test/BasicTest.java
+++ test/BasicTest.java
@@ -1,35 +1,37 @@
 import org.junit.*;
+
 import java.util.*;
+
 import play.test.*;
 import models.*;
 
 public class BasicTest extends UnitTest {
 
     @Before
-    public void setup(){
+    public void setup() {
         Fixtures.deleteDatabase();
     }
 
     @Test
-    public void createPost(){
+    public void createPost() {
         // Create a new user and save it
-        User bob = new User("bob@gmail.com","secrete","Bob").save();
+        User bob = new User("bob@gmail.com", "secrete", "Bob").save();
 
         // Create a new post
-        new Post(bob,"My first post","Hello world").save();
+        new Post(bob, "My first post", "Hello world").save();
 
         // Test that the post has been created
-        assertEquals(1,Post.count());
+        assertEquals(1, Post.count());
 
         // Retrieve all posts created by Bob
-        List<Post> bobPosts = Post.find("byAuthor",bob).fetch();
+        List<Post> bobPosts = Post.find("byAuthor", bob).fetch();
 
         // Tests
-        assertEquals(1,bobPosts.size());
+        assertEquals(1, bobPosts.size());
         Post firstPost = bobPosts.get(0);
         assertNotNull(firstPost);
-        assertEquals(bob,firstPost.author);
-        assertEquals("My first post",firstPost.title);
+        assertEquals(bob, firstPost.author);
+        assertEquals("My first post", firstPost.title);
         assertNotNull(firstPost.postedAt);
     }
 
@@ -113,6 +115,38 @@ public class BasicTest extends UnitTest {
         frontPost.addComment("Jim", "Hello guys");
         assertEquals(3, frontPost.comments.size());
         assertEquals(4, Comment.count());
+    }
+
+    @Test
+    public void testTags() {
+        //Create a new user and save it
+        User bob = new User("bob@gmail.com", "secret", "Bob").save();
+
+        //Create a new post
+        Post bobPost = new Post(bob, "My first post", "Hellow world").save();
+        Post anotherBobPost = new Post(bob, "Hop", "Helli world").save();
+
+        // Well
+        assertEquals(0, Post.findTaggedWith("Red").size());
+
+        // Tag it now
+        bobPost.tagItWith("Red").tagItWith("Blue").save();
+        anotherBobPost.tagItWith("Red").tagItWith("Green").save();
+
+        // Check
+        assertEquals(2, Post.findTaggedWith("Red").size());
+        assertEquals(1, Post.findTaggedWith("Blue").size());
+        assertEquals(1, Post.findTaggedWith("Green").size());
+        assertEquals(1, Post.findTaggedWith("Red", "Blue").size());
+        assertEquals(1, Post.findTaggedWith("Red", "Green").size());
+        assertEquals(0, Post.findTaggedWith("Red", "Green", "Blue").size());
+        assertEquals(0, Post.findTaggedWith("Green", "Blue").size());
+
+        List<Map> cloud = Tag.getCloud();
+        assertEquals(
+                "[{pound=1, tag=Blue}, {pound=1, tag=Green}, {pound=2, tag=Red}]",
+                cloud.toString()
+        );
     }
 
 }
